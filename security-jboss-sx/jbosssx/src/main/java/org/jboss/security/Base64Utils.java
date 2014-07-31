557df9aa740d84b8c10896e4b89b31518b44d3b6
Josef Cacek <josef.cacek@gmail.com>
2014-07-31 15:47:32 +0200

    SECURITY-851 fix stripping leading zeroes in Base64Utils class

--- security-jboss-sx/jbosssx/src/main/java/org/jboss/security/Base64Utils.java
+++ security-jboss-sx/jbosssx/src/main/java/org/jboss/security/Base64Utils.java
@@ -1,184 +1,131 @@
 /*
-* JBoss, Home of Professional Open Source
-* Copyright 2005, JBoss Inc., and individual contributors as indicated
-* by the @authors tag. See the copyright.txt in the distribution for a
-* full listing of individual contributors.
-*
-* This is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Lesser General Public License as
-* published by the Free Software Foundation; either version 2.1 of
-* the License, or (at your option) any later version.
-*
-* This software is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this software; if not, write to the Free
-* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
-*/
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.security;
 
-/** Base64 encoding/decoding utilities
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Base64 encoding/decoding utilities. This implementation is not MIME compliant (rfc1421). The padding in this implementation
+ * (if used) is a prefix of the output.
  * 
  * @author Scott.Stark@jboss.org
- * @version $Revison:$
+ * @author Josef Cacek
  */
 public class Base64Utils
 {
-   private static final char[] base64Table =
-   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz./".toCharArray();
+
+   private static final String base64Str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz./";
+   private static final char[] base64Table = base64Str.toCharArray();
    public static final String BASE64_ENCODING = "BASE64";
    public static final String BASE16_ENCODING = "HEX";
+   public static final char PAD = '_';
+   public static final String REGEX = "^" + PAD + "{0,2}[" + base64Str + "]*$";
 
-   // These functions assume that the byte array has MSB at 0, LSB at end.
-   // Reverse the byte array (not the String) if this is not the case.
-   // All base64 strings are in natural order, least significant digit last.
    public static String tob64(byte[] buffer)
    {
-      boolean notleading = false;
+      return tob64(buffer, false);
+   }
+
+   public static String tob64(byte[] buffer, boolean usePadding)
+   {
       int len = buffer.length, pos = len % 3, c;
       byte b0 = 0, b1 = 0, b2 = 0;
       StringBuffer sb = new StringBuffer();
 
-      switch(pos)
+      int i = 0;
+      if (usePadding)
       {
-         case 1:
-            b2 = buffer[0];
-            break;
+         for (i = pos; i != 0; i = (i + 1) % 3)
+         {
+            sb.append(PAD);
+         }
+         i = 0;
+      }
+      switch (pos)
+      {
          case 2:
-            b1 = buffer[0];
-            b2 = buffer[1];
+            b1 = buffer[i++];
+            c = ((b0 & 3) << 4) | ((b1 & 0xf0) >>> 4);
+            sb.append(base64Table[c]);
+         case 1:
+            b2 = buffer[i++];
+            c = ((b1 & 0xf) << 2) | ((b2 & 0xc0) >>> 6);
+            sb.append(base64Table[c]);
+            c = b2 & 0x3f;
+            sb.append(base64Table[c]);
             break;
       }
-      do
-      {
-         c = (b0 & 0xfc) >>> 2;
-         if(notleading || c != 0)
-         {
-            sb.append(base64Table[c]);
-            notleading = true;
-         }
-         c = ((b0 & 3) << 4) | ((b1 & 0xf0) >>> 4);
-         if(notleading || c != 0)
-         {
-            sb.append(base64Table[c]);
-            notleading = true;
-         }
-         c = ((b1 & 0xf) << 2) | ((b2 & 0xc0) >>> 6);
-         if(notleading || c != 0)
-         {
-            sb.append(base64Table[c]);
-            notleading = true;
-         }
-         c = b2 & 0x3f;
-         if(notleading || c != 0)
-         {
-            sb.append(base64Table[c]);
-            notleading = true;
-         }
-         if(pos >= len)
-            break;
-         else
-         {
-            try
-            {
-               b0 = buffer[pos++];
-               b1 = buffer[pos++];
-               b2 = buffer[pos++];
-            }
-            catch(ArrayIndexOutOfBoundsException e)
-            {
-               break;
-            }
-         }
-      } while(true);
 
-      if(notleading)
-         return sb.toString();
-      else
-         return "0";
+      while (pos < len)
+      {
+         b0 = buffer[pos++];
+         b1 = buffer[pos++];
+         b2 = buffer[pos++];
+         c = (b0 & 0xfc) >>> 2;
+         sb.append(base64Table[c]);
+         c = ((b0 & 3) << 4) | ((b1 & 0xf0) >>> 4);
+         sb.append(base64Table[c]);
+         c = ((b1 & 0xf) << 2) | ((b2 & 0xc0) >>> 6);
+         sb.append(base64Table[c]);
+         c = b2 & 0x3f;
+         sb.append(base64Table[c]);
+      }
+
+      return sb.toString();
    }
 
    public static byte[] fromb64(String str) throws NumberFormatException
    {
-      int len = str.length();
-      if(len == 0)
-         throw PicketBoxMessages.MESSAGES.invalidEmptyBase64String();
-
-      byte[] a = new byte[len + 1];
-      char c;
-      int i, j;
-
-      for(i = 0; i < len; ++i)
+      if (str.length() == 0)
       {
-         c = str.charAt(i);
-         try
+         return new byte[0];
+      }
+
+      while (str.length() % 4 != 0)
+      {
+         str = PAD + str;
+      }
+      if (!str.matches(REGEX))
+      {
+         throw PicketBoxMessages.MESSAGES.invalidBase64String(str);
+      }
+      ByteArrayOutputStream bos = new ByteArrayOutputStream((str.length() * 3) / 4);
+      for (int i = 0, n = str.length(); i < n;)
+      {
+         int pos0 = base64Str.indexOf(str.charAt(i++));
+         int pos1 = base64Str.indexOf(str.charAt(i++));
+         int pos2 = base64Str.indexOf(str.charAt(i++));
+         int pos3 = base64Str.indexOf(str.charAt(i++));
+         if (pos0 > -1)
          {
-            for(j = 0; c != base64Table[j]; ++j)
-               ;
-         } catch(Exception e)
-         {
-            throw PicketBoxMessages.MESSAGES.illegalBase64Character();
+            bos.write(((pos1 & 0x30) >>> 4) | (pos0 << 2));
          }
-         a[i] = (byte) j;
-      }
-
-      i = len - 1;
-      j = len;
-      try
-      {
-         while(true)
+         if (pos1 > -1)
          {
-            a[j] = a[i];
-            if(--i < 0)
-               break;
-            a[j] |= (a[i] & 3) << 6;
-            --j;
-            a[j] = (byte) ((a[i] & 0x3c) >>> 2);
-            if(--i < 0)
-               break;
-            a[j] |= (a[i] & 0xf) << 4;
-            --j;
-            a[j] = (byte) ((a[i] & 0x30) >>> 4);
-            if(--i < 0)
-               break;
-            a[j] |= (a[i] << 2);
-
-            // Nasty, evil bug in Microsloth's Java interpreter under
-            // Netscape:  The following three lines of code are supposed
-            // to be equivalent, but under the Windows NT VM (Netscape3.0)
-            // using either of the two commented statements would cause
-            // the zero to be placed in a[j] *before* decrementing j.
-            // Weeeeird.
-            a[j-1] = 0; --j;
-            // a[--j] = 0;
-            // --j; a[j] = 0;
-
-            if(--i < 0)
-               break;
+            bos.write(((pos2 & 0x3c) >>> 2) | ((pos1 & 0xf) << 4));
          }
+         bos.write(((pos2 & 3) << 6) | pos3);
       }
-      catch(Exception e)
-      {
-
-      }
-
-      try
-      {
-         while(a[j] == 0)
-            ++j;
-      }
-      catch(Exception e)
-      {
-         return new byte[1];
-      }
-
-      byte[] result = new byte[len - j + 1];
-      System.arraycopy(a, j, result, 0, len - j + 1);
-      return result;
+      return bos.toByteArray();
    }
 
 }
---
+++ security-jboss-sx/jbosssx/src/test/java/org/jboss/test/Base64UtilsTestCase.java
@@ -0,0 +1,92 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+
+import org.jboss.security.Base64Utils;
+import org.junit.Test;
+
+/**
+ * Tests for {@link Base64Utils} class.
+ * 
+ * @author Josef Cacek
+ */
+public class Base64UtilsTestCase
+{
+
+   @Test
+   public void leadingZeroKeyTest()
+   {
+      final byte[] leadingZeroKey = new byte[] { 0, 81, 121, -37, 46, -64, 20, 114 };
+      final String b64EncLeadinZeroKey = "01HUTikm1Ho";
+      assertEquals(b64EncLeadinZeroKey, Base64Utils.tob64(leadingZeroKey));
+      assertArrayEquals(leadingZeroKey, Base64Utils.fromb64(b64EncLeadinZeroKey));
+   }
+
+   @Test
+   public void paddedLeadingZeroKeyTest()
+   {
+      final byte[] key = new byte[] { 0, 81, 121, -37, 46, -64, 20, 114 };
+      final String b64Key = "_01HUTikm1Ho";
+      assertEquals(b64Key, Base64Utils.tob64(key, true));
+      assertArrayEquals(key, Base64Utils.fromb64(b64Key));
+   }
+
+   @Test
+   public void lastZeroTest()
+   {
+      final byte[] key = new byte[] { 114, 81, 121, -37, 46, -64, 20, 0 };
+      final String b64Key = "79HUTikm1G0";
+      assertEquals(b64Key, Base64Utils.tob64(key));
+      assertArrayEquals(key, Base64Utils.fromb64(b64Key));
+   }
+
+   @Test
+   public void emptyKeyTest()
+   {
+      final byte[] key = new byte[] {};
+      final String b64Key = "";
+      assertEquals(b64Key, Base64Utils.tob64(key, true));
+      assertEquals(b64Key, Base64Utils.tob64(key, false));
+      assertArrayEquals(key, Base64Utils.fromb64(b64Key));
+   }
+
+   @Test
+   public void paddingTest()
+   {
+      ByteArrayOutputStream baos = new ByteArrayOutputStream(4);
+      baos.write(0);
+      assertEquals("__00", Base64Utils.tob64(baos.toByteArray(), true));
+      baos.write(0);
+      assertEquals("_000", Base64Utils.tob64(baos.toByteArray(), true));
+      baos.write(0);
+      assertEquals("0000", Base64Utils.tob64(baos.toByteArray(), true));
+      baos.write(0);
+      assertEquals("__000000", Base64Utils.tob64(baos.toByteArray(), true));
+   }
+
+}
